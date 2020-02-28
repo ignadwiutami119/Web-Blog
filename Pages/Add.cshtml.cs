393e96f12f32f98a27355c1e269e24c36892c8d5
Igna Dwi Utami <Igna.Utami@bsi.co.id>
2020-02-28 16:50:45 +0700

    add upload image feature

--- Pages/Add.cshtml.cs
+++ Pages/Add.cshtml.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,54 +13,53 @@ using Microsoft.Extensions.Logging;
 using WebApp.Data;
 using WebApp.Models;
 
-namespace WebApp.Pages
-{
+namespace WebApp.Pages {
     [Authorize]
-    public class AddModel : PageModel
-    {
+    public class AddModel : PageModel {
         private readonly ILogger<AddModel> _logger;
         private readonly UserManager<IdentityUser> _UserManager;
         private readonly SignInManager<IdentityUser> _SignInManager;
+        private readonly IWebHostEnvironment _Environtment;
 
-        public AddModel(
+        public AddModel (
+            IWebHostEnvironment environment,
             ILogger<AddModel> logger,
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ApplicationDbContext applicationDbContext
-        ){
+        ) {
             _logger = logger;
             _UserManager = userManager;
             _SignInManager = signInManager;
             _appDbContext = applicationDbContext;
+            _Environtment = environment;
         }
 
         private ApplicationDbContext _appDbContext;
 
-        public void OnGet()
-        {
+        public void OnGet () {
             var get = from a in _appDbContext.Articles select a;
             ViewData["Data"] = get;
         }
 
-        public void OnPost(string title, string highlight, string content, string image)
-        {
-            Console.WriteLine(highlight);
-            Console.WriteLine(content);
-            Console.WriteLine(image);
-            Console.WriteLine(title);
-            Console.WriteLine("WOY");
-           Articles articles = new Articles(){
-               title = title,
-               highlight = highlight,
-               content = content,
-               image = image,
-               Published_at = DateTime.Now,
-               Edited_at = DateTime.Now
-           };
-           _appDbContext.Add(articles);
-           _appDbContext.SaveChanges();
-           var get = from a in _appDbContext.Articles select a;
-           ViewData["Data"] = get;
+        public void OnPost (string title, string highlight, string content, IFormFile image) {
+            var path = "wwwroot//image";
+            Directory.CreateDirectory(path);
+            var Filename = Path.Combine (path, Path.GetFileName (image.FileName));
+            image.CopyTo (new FileStream (Filename, FileMode.Create));
+            var file = Filename.Substring(8).Replace(@"\","/");
+            Articles articles = new Articles () {
+                title = title,
+                highlight = highlight,
+                content = content,
+                image = file,
+                Published_at = DateTime.Now,
+                Edited_at = DateTime.Now
+            };
+            _appDbContext.Add (articles);
+            _appDbContext.SaveChanges ();
+            var get = from a in _appDbContext.Articles select a;
+            ViewData["Data"] = get;
         }
     }
 }
